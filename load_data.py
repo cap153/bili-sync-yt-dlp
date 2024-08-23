edb83c76f519fc60dd97ced53bb0e04f4df4a4f4
cap153 <67049883+cap153@users.noreply.github.com>
2024-08-23 20:29:18 +0800

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -18,9 +18,5 @@ class SQLiteManager:
         self.cursor.execute(f'INSERT OR IGNORE INTO "{table_name}" (bvid) VALUES (?)', (value,))
 
     def get_values(self, table_name):
-        # 检查表是否存在，表不存在，返回空集合
-        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table_name,))
-        if not self.cursor.fetchone():
-            return set()
-        self.cursor.execute(f'SELECT bvid FROM "{table_name}"')
+        self.cursor.execute(f'''CREATE TABLE IF NOT EXISTS "{table_name}" (bvid TEXT UNIQUE)''')
         return set(row[0] for row in self.cursor.fetchall())
